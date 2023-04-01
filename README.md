b8a9453a9bd00fbfb29754a7b79f3f8a97abc2a5
Stef Maree <maree.stef@kpmg.nl>
2023-04-01 22:32:10 +0200

    Repo setup

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# python-code-puzzles
+# Pyzzle
+A collection of code snippets and scripts I use when solving python coding puzzles and other nerdy things.
+I tried my best to include references to the sources of code snippets, but I might have missed some,
+so just let me know.
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+setup(
+    author="scmaree",
+    author_email='',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.10',
+    ],
+    description="",
+    include_package_data=True,
+    keywords='pyzzle',
+    name='pyzzle',
+    packages=find_packages(include=["pyzzle"]),
+    setup_requires=[],
+    version='0.1.0',
+    zip_safe=False,
+)
